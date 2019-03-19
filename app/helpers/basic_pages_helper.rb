78ff24c896b37ae52cf3a931c8f41538e4d55d49
traxbay <xare92@hotmail.com>
2019-03-19 14:35:07 +0300

    modifying titles for all pages

--- app/helpers/basic_pages_helper.rb
+++ app/helpers/basic_pages_helper.rb
@@ -1,2 +1,11 @@
 module BasicPagesHelper
+
+  def full_title(page_title = "")
+    base_title = "Service Ordering System"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/basic_pages_controller_test.rb
+++ test/controllers/basic_pages_controller_test.rb
@@ -5,19 +5,26 @@ class BasicPagesControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  def setup
+    @base_title = "Service Ordering System"
+  end
+
   test "should get home" do
    get basic_pages_home_url
    assert_response :success
+   assert_select "title", "Home | #{@base_title}"
  end
 
   test "should get about" do
     get basic_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get basic_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 
