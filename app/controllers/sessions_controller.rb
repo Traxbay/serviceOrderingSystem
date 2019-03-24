e59ba60d9edc45cc7d712c245ba4943e78d88dfe
traxbay <xare92@hotmail.com>
2019-03-24 14:41:28 +0300

    syntax error

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # login the user
+      flash.now[:success] = "Welcome to Service Ordering System ! "
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "Error logging in !, invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+
+end
