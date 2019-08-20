494fab710b5e0ead7321cceefb0f79713568c31a
abdullah faraj <xare92@hotmail.com>
2019-08-20 03:11:48 +0300

    redirect new users to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to Service Ordering System !"
-      redirect_to @user
+      redirect_to root_path
     else
       render 'new'
     end
